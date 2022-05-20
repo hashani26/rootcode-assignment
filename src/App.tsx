b480434fab9c62fd515278417b351ea298d0014f
hashanikithulgoda <hashanilakshi@gmail.com>
2022-05-20 11:45:07 +0530

    fix eslint error when importing React with eslint react plugin and add vscode settings to auto format on save

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import React from 'react'
 import './App.css'
 
 function App() {
-    return (
-        <div className="App">
-      Test
-        </div>
-    )
+    return <div className="App">Test</div>
 }
 
 export default App
